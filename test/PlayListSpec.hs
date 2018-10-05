cbb13dff6f665d4d0567b2e1a7c5ad2caf074ec4
Kenny Shen <kenny@machinesung.com>
2018-10-05 10:41:04 +0800

    Run tests on Travis
    Addresses #8

--- test/PlayListSpec.hs
+++ test/PlayListSpec.hs
@@ -6,15 +6,40 @@ import           PlayList
 
 spec :: Spec
 spec = do
-  describe "readPlayList" $ do
-    it "parses a YAML file" $ do
-      readPlayList "/home/revtintin/labs/so-fulla/playlist.yml"
-        `shouldReturn`
-          Album {name = "Foo", artist = "Bar", location = "/home/foo/music", songs = Just ["baa-baa.flac","baz.flac"]} :
-          Album {name = "Bar", artist = "Qux", location = "/home/bar/music", songs = Just ["abc.flac","efg.flac"]} :
-          []
-
-  describe "playListToSource" $ do
+  describe "readPlayList" $
+    it "parses a YAML file" $
+      readPlayList "test/Golden/playlist.yml"
+        `shouldReturn` [
+            Album {
+                name = "Foo"
+              , artist = "Bar"
+              , location = "/home/foo/music"
+              , songs = Just [
+                 "baa-baa.flac"
+                ,"baz.flac"
+                ]
+              }
+          , Album {
+                name = "Bar"
+              , artist = "Qux"
+              , location = "/home/bar/music"
+              , songs = Just [
+                 "abc.flac"
+                ,"efg.flac"
+                ]
+              }
+          ]
+  describe "playListToSource" $
     it "takes a playlist and returns the filepaths" $ do
-      let albums = [Album {name = "Bar", artist = "Qux", location = "/home/bar/music", songs = Just ["abc.flac","efg.flac"]}]
+      let albums = [
+            Album {
+                name = "Bar"
+              , artist = "Qux"
+              , location = "/home/bar/music"
+              , songs = Just [
+                  "abc.flac"
+                 ,"efg.flac"
+                ]
+              }
+            ]
       playListToSource albums `shouldReturn` ["/home/bar/music/abc.flac", "/home/bar/music/efg.flac"]
